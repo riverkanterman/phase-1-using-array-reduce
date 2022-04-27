ba97776a1a9a5bca3eb993dda67ceee513c68e69
River <+>
2022-04-27 19:24:18 -0400

    commit tree 69f1d380f4182f39d068a4a9ae9cc9c734eab1c5

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue,
+)
+
+console.log(totalBatteries)
+
+
+/*const sum = batteryBatches.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+     totalBatteries
+ );
+
+ console.log(sum)*/
